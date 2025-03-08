db97052af2b43a8eb21b3922b0f37a34149b6947
Owen-Yator <yatorowen@gmail.com>
2025-03-08 23:56:35 +0300

    AddNoteActivity.kt

--- app/src/main/java/com/example/notessqlite/AddNoteActivity.kt
+++ app/src/main/java/com/example/notessqlite/AddNoteActivity.kt
@@ -5,11 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.notessqlite.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NotesDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_note)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
 
         }
     }
