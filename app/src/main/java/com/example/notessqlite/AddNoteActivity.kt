1cb55b4ac4dabbff4b087c4415ff2cf5eb0445e7
Owen-Yator <yatorowen@gmail.com>
2025-03-10 10:34:23 +0300

    AddNoteActivity.kt thus file enables the user to add a note to the app and save it to the database by clicking the save button.

--- app/src/main/java/com/example/notessqlite/AddNoteActivity.kt
+++ app/src/main/java/com/example/notessqlite/AddNoteActivity.kt
@@ -1,6 +1,7 @@
 package com.example.notessqlite
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +19,15 @@ class AddNoteActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         db = NotesDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.toString()
+            val note = Note(0,title,content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
+        }
 
         }
     }
